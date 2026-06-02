e2d419ab6f340f16a270949b3ea4a71f4fc89450
Pradyumna-Singh729 <sattire729@gmail.com>
2026-06-02 19:32:17 +0530

    Completed 6th chapter of Python CC upto the last problem set

---
+++ Python-Crash-Course/06_Dictionaries/practice_problems/try_it_yourself_03_6-7_People.py
@@ -0,0 +1,14 @@
+# 6-7 People: Start with the program you wrote for Exercise 6-1 (page 102). Make two new dictionaries representing different people, and store all three dictionaries in a list called people.
+# Loop through your list of people. As you loop through the list, print everything you know about each person.
+
+information = {'First Name': 'Furina',
+    'Last Name': 'de Fontaine',
+    'age': '500',
+    'city': 'Fontaine'
+    }
+
+
+print(information['First Name'])
+print(information['Last Name'])
+print(information['age'])
+print(information['city'])

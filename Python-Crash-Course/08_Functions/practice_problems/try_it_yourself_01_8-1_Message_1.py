9bdaccb83613196feaffcb2f4290459c970f1cb7
Pradyumna-Singh729 <sattire729@gmail.com>
2026-06-16 06:29:05 +0530

    Started 8th chapter of Python CC and understood the material upto equivalent function calls

---
+++ Python-Crash-Course/08_Functions/practice_problems/try_it_yourself_01_8-1_Message_1.py
@@ -0,0 +1,9 @@
+# 8-1 Message: Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the function, and make sure the message displays correctly.
+
+def display_message():
+    """Prints a sentence telling everyone what they are 
+    learning about in this chapter.
+    """
+    print("In this chapter, I am learning about functions in Python.")
+
+display_message()
---
+++ Python-Crash-Course/08_Functions/sandbox/07-default_values_1.py
@@ -0,0 +1,8 @@
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(pet_name='willie')
+describe_pet('willie')
+describe_pet(pet_name='harry', animal_type='hamster') 
